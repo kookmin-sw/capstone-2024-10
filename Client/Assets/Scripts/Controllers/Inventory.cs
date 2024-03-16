e03c91652d5c1fbd2c5b07c14c31f67ec48daf94
Kim Taebum <ddino99@naver.com>
2024-03-16 17:20:22 +0900

    Inventory 클래스 구현 중

---
+++ Client/Assets/Scripts/Controllers/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Fusion;
+
+public class Inventory: NetworkBehaviour
+{
+    public Crew Crew { get; protected set; }
+    public Define.CreatureState CreatureState => Crew.CreatureState;
+    public Define.CreaturePose CreaturePose => Crew.CreaturePose;
+
+    public List<BaseItem> Items;
+
+    public override void Spawned()
+    {
+        Init();
+    }
+
+    protected virtual void Init()
+    {
+        Crew = gameObject.GetComponent<Crew>();
+
+        Items = new List<BaseItem>();
+    }
+
+    public void CheckAndGet(NetworkBehaviour item)
+    {
+
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    public void Rpc_Get(NetworkBehaviour item)
+    {
+
+    }
+
+    protected void Use()
+    {
+
+    }
+}
