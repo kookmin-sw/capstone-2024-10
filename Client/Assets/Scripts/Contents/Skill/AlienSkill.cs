05aecb1491a04b67bb72cbaed3797782f9ef219b
jangminseok-05 <55117195+jangminseok-05@users.noreply.github.com>
2024-03-21 20:05:32 +0900

    feat: AlienSkill 및 AlienAnimController 수정
    AlienSkill에서 attack 방식 정의
    AlienAnimController 수정

---
+++ Client/Assets/Scripts/Contents/Skill/AlienSkill.cs
@@ -0,0 +1,36 @@
+using Fusion;
+using System.Buffers;
+using UnityEngine;
+
+public class AlienSkill : BaseSkill
+{
+    [SerializeField] private float attackRange = 2f; // 근접 공격 범위
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+
+    public override void Rpc_Use()
+    {
+        try
+        {
+            // 주인공 캐릭터의 위치와 방향을 기준으로 공격 범위 내의 대상을 찾음
+            Collider[] hitColliders = Physics.OverlapSphere(Owner.transform.position, attackRange);
+
+            // 찾은 대상에 대해 각각 데미지를 입히는 동작 수행
+            foreach (Collider col in hitColliders)
+            {
+                // 대상이 공격자 자신이 아니고, 대상이 Crew 클래스를 가진 경우
+                if (col.gameObject != Owner.gameObject && col.gameObject.TryGetComponent(out Crew crew))
+                {
+                    // 대상에게 데미지를 입히는 함수 호출
+                    crew.OnDamaged(Owner.AlienStat.Damage);
+                    Debug.Log(crew.DataId + " take damage");
+                }
+            }
+        }
+        catch (System.NullReferenceException ex)
+        {
+            Debug.Log("Attack Miss : NullReferenceException caught: " + ex.Message);
+            // 예외 처리
+        }
+    }
+}
