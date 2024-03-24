0a279ba2843d52948abd4e4caea3d02900f4160c
Hamsuyeon <hammun6@kookmin.ac.kr>
2024-03-25 04:31:25 +0900

    Crew animator 수정
    8방향으로 자연스럽게 달리기가 가능하게 Crew animator 구조 수정

--- Client/Assets/Scripts/Contents/AnimController/CrewAnimController.cs
+++ Client/Assets/Scripts/Contents/AnimController/CrewAnimController.cs
@@ -4,22 +4,19 @@ using Fusion;
 public class CrewAnimController : BaseAnimController
 {
     [Networked] public float SitParameter { get; protected set; }
+    public CrewStat CrewStat { get; protected set; }
 
     protected override void Init()
     {
         base.Init();
         CrewStat = gameObject.GetComponent<CrewStat>();
-
-        SetFloat("Action", 0);
-        SetFloat("Health", 100);
-        SetFloat("Sit", 0);
-        SetFloat("sitSpeed", 0);
     }
 
     #region Update
 
     protected override void PlayIdle()
     {
+        //SetFloat("Health", CrewStat.Hp);
         switch (CreaturePose)
         {
             case Define.CreaturePose.Stand:
@@ -40,22 +37,7 @@ public class CrewAnimController : BaseAnimController
 
     protected override void PlayMove()
     {
-        SetFloat("Health", CrewStat.Hp);
-        //switch (CreaturePose)
-        //{
-        //    case Define.CreaturePose.Stand:
-        //        CrewSitDownOrUp(0);
-        //        CrewStandMove(1.5f);
-        //        break;
-        //    case Define.CreaturePose.Sit:
-        //        CrewSitDownOrUp(1);
-        //        CrewSitMove(1);
-        //        break;
-        //    case Define.CreaturePose.Run:
-        //        CrewStandMove(2);
-        //        break;
-        //}
-
+        //SetFloat("Health", CrewStat.Hp);
         switch (CreaturePose)
         {
             case Define.CreaturePose.Stand:
@@ -82,7 +64,7 @@ public class CrewAnimController : BaseAnimController
 
     public void PlayInteract()
     {
-        // TODO
+        
     }
 
     public void PlayUseItem()
@@ -97,60 +79,4 @@ public class CrewAnimController : BaseAnimController
 
     #endregion
 
-    #region CrewAnim
-    private void CrewSitDownOrUp(float value)
-    {
-        float smoothness = 5f; // 조절 가능한 부드러움 계수
-        SitDown = Mathf.Lerp(SitDown, value, Runner.DeltaTime * smoothness);
-        SetFloat("Sit", SitDown);
-    }
-    private void CrewSitMove(float value)
-    {
-        float smoothness = 5f; // 조절 가능한 부드러움 계수
-        SitSpeedParameter = Mathf.Lerp(SitSpeedParameter, value, Runner.DeltaTime * smoothness);
-        SetFloat("sitSpeed", SitSpeedParameter);
-    }
-    private void CrewStandMove(float value)
-    {
-        float smoothness = 5f; // 조절 가능한 부드러움 계수
-        StandSpeedParameter = Mathf.Lerp(StandSpeedParameter, value, Runner.DeltaTime * smoothness);
-        SetFloat("moveSpeed", StandSpeedParameter);
-    }
-    private void CrewMoveDirection(float value)
-    {
-        float smoothness = 5f; // 조절 가능한 부드러움 계수
-        MoveDirectionParameter = Mathf.Lerp(MoveDirectionParameter, value, Runner.DeltaTime * smoothness);
-        SetFloat("moveDirection", MoveDirectionParameter);
-    }
-    #endregion
-
-    #region Input
-    private void InputHandle()
-    {
-        VerticalInput = Input.GetAxis("Vertical");
-        HorizontalInput = Input.GetAxis("Horizontal");
-        if (VerticalInput > 0)
-        {
-            // 상키를 눌렀을 때 A 애니메이션 재생
-            CrewMoveDirection(3);
-        }
-        else if (VerticalInput < 0)
-        {
-            // 하키를 눌렀을 때 A 애니메이션 재생
-            CrewMoveDirection(0);
-        }
-
-        // 좌,우키 입력에 따라 애니메이션 재생
-        if (HorizontalInput > 0)
-        {
-            // 우키를 눌렀을 때 B 애니메이션 재생
-            CrewMoveDirection(2);
-        }
-        else if (HorizontalInput < 0)
-        {
-            // 좌키를 눌렀을 때 B 애니메이션 재생
-            CrewMoveDirection(1);
-        }
-    }
-    #endregion
 }
