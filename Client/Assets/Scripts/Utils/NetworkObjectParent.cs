eb419704abcdec7647e9e00a80c719b81d4fd570
kkilme <jojo5142@naver.com>
2024-05-02 16:46:39 +0900

    item spawn point on sectors

---
+++ Client/Assets/Scripts/Utils/NetworkObjectParent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class NetworkObjectParent : NetworkBehaviour
+{
+    // 네트워크 오브젝트의 부모 설정을 위한 Dummy Script
+}
