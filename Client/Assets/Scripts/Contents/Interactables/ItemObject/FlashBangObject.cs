3d73a739040ac8f804342ab8313dfa87f5a19603
Kim Taebum <39542671+scarleter99@users.noreply.github.com>
2024-06-24 17:00:43 +0900

    feat: Flashbang 파티클 이펙트 추가 (#170)

--- Client/Assets/Scripts/Contents/Interactables/ItemObject/FlashBangObject.cs
+++ Client/Assets/Scripts/Contents/Interactables/ItemObject/FlashBangObject.cs
@@ -6,14 +6,23 @@ public class FlashBangObject : BaseItemObject
     public override int DataId => Define.ITEM_FLASHBANG_ID;
     public AudioSource AudioSource { get; protected set; }
 
+    public GameObject ExplosionParticle { get; protected set; }
+
+    public override void FixedUpdateNetwork()
+    {
+        ExplosionParticle.transform.rotation = Quaternion.identity;
+    }
+
     public override void Init()
     {
         AudioSource = GetComponent<AudioSource>();
+        ExplosionParticle = Util.FindChild(gameObject, "Explosion");
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void Rpc_Explode()
     {
+        ExplosionParticle.SetActive(true);
         gameObject.SetLayerRecursive(LayerMask.NameToLayer("MapObject"));
     }
 
