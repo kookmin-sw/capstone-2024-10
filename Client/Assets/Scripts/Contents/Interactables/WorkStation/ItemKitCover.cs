1cadf493a3886c40c10b104b6d951866e773596a
Kim Taebum <ddino99@naver.com>
2024-07-21 19:17:32 +0900

    feat: ItemKitCover 추가
    ItemKit 뚜껑에도 Collider 추가

---
+++ Client/Assets/Scripts/Contents/Interactables/WorkStation/ItemKitCover.cs
@@ -0,0 +1,18 @@
+public class ItemKitCover : BaseWorkStation
+{
+    public ItemKit ItemKit { get; set; }
+
+    public override bool IsInteractable(Creature creature)
+    {
+        return ItemKit.IsInteractable(creature);
+    }
+
+    public override bool Interact(Creature creature)
+    {
+        return ItemKit.IsInteractable(creature);
+    }
+
+    protected override void Rpc_WorkComplete() { }
+
+    protected override void Rpc_PlaySound() { }
+}
