8a774422cd425b43d24f64d9ffbe42becda82a7c
Kim Taebum <ddino99@naver.com>
2024-07-23 22:18:51 +0900

    hotfix: 인원수 정상화

--- Client/Assets/Scripts/Utils/Define.cs
+++ Client/Assets/Scripts/Utils/Define.cs
@@ -195,7 +195,7 @@ public static class Define
 
     #region Value
 
-    public const int PLAYER_COUNT = 2;
+    public const int PLAYER_COUNT = 4;
     public const int MAX_ITEM_NUM = 4;
     public const int MAX_SKILL_NUM = 4;
 
