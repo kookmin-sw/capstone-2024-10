19a7e7d262f59c1de856e931b91f461b6c359de9
jmJang00 <50986275+jmJang00@users.noreply.github.com>
2024-09-11 02:19:29 +0900

    hotfix: 해상도 선택 옵션 변경 (#256)
    * 해상도 선택 옵션 변경
    * MapUI 버그 수정

--- Client/Assets/Scripts/Systems/SettingSystem.cs
+++ Client/Assets/Scripts/Systems/SettingSystem.cs
@@ -1,13 +1,15 @@
+using System;
 using UnityEngine;
 
 public class SettingSystem : MonoBehaviour
 {
     #region Screen
-    public int Width { get; set; } = 1920;
-    public int Height { get; set; } = 1080;
-    public bool Fullscreen { get; set; } = true;
-    private int _quality = 1;
+    public int Width { get; set; } = 1280;
+    public int Height { get; set; } = 720;
+    public int Fullscreen { get; set; } = 0;
+    private int _quality = 3;
     public int VSycn = 0;
+    public int ScreenRatioIndex = 0;
     public int Quality
     {
         get { return _quality; }
@@ -18,6 +20,13 @@ public class SettingSystem : MonoBehaviour
             _quality = value;
         }
     }
+
+    private int[,] _screenResolution = new int[,]
+    {
+        {1280, 720},
+        {1920, 1080},
+        {2560, 1440},
+    };
     #endregion
 
     #region Input
@@ -29,10 +38,12 @@ public class SettingSystem : MonoBehaviour
         Managers.GameMng.SettingSystem = this;
 
         Sensitivity = PlayerPrefs.GetFloat("Sensitivity", 1.0f);
-        Width = PlayerPrefs.GetInt("ScreenWidth", 1920);
-        Height = PlayerPrefs.GetInt("ScreenHeight", 1080);
-        Quality = PlayerPrefs.GetInt("Textures", 1);
+        Width = PlayerPrefs.GetInt("ScreenWidth", 1280);
+        Height = PlayerPrefs.GetInt("ScreenHeight", 720);
+        Quality = PlayerPrefs.GetInt("Textures", 3);
         VSycn = PlayerPrefs.GetInt("VSync", 0);
+        ScreenRatioIndex = PlayerPrefs.GetInt("ScreenRatio", 0);
+        Fullscreen = PlayerPrefs.GetInt("FullScreen", 0);
     }
 
     private void SetQuality(int index)
@@ -53,19 +64,29 @@ public class SettingSystem : MonoBehaviour
         Debug.Log($"{field}: {PlayerPrefs.GetFloat(field)}");
     }
 
+    public void SelectResolution(int idx)
+    {
+        int width = _screenResolution[idx, 0];
+        int height = _screenResolution[idx, 1];
+        Debug.Log($"{width} x {height}");
+        SetResolution(width, height);
+        ScreenRatioIndex = idx;
+        PlayerPrefs.SetInt("ScreenRatio", idx);
+    }
+
     public void SetResolution(int width, int height)
     {
         Width = width;
         Height = height;
         PlayerPrefs.SetInt("ScreenWidth", width);
         PlayerPrefs.SetInt("ScreenHeight", height);
-        Screen.SetResolution(Width, Height, Fullscreen);
+        Screen.SetResolution(Width, Height, Convert.ToBoolean(Fullscreen));
     }
 
     public void SetFullScreen(bool fullscreen)
     {
-        Fullscreen = fullscreen;
-        Screen.SetResolution(Width, Height, Fullscreen);
+        Fullscreen = fullscreen ? 1 : 0;
+        Screen.SetResolution(Width, Height, fullscreen);
     }
 
     public void SetMouseSensitivity(float sliderValueSensitivity)
